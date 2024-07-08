ea6a90340b4a4fedac0e53c6571cbcdc5e199991
0ju1c3 <otapadia@gmail.com>
2024-07-08 19:08:06 +0530

    feat: updated userModel

--- models/user.model.js
+++ models/user.model.js
@@ -1,16 +1,30 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 const { model, Schema } = mongoose;
 
 const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  otp: String,
+  role: { type: String, default: "User" },
   createdAt: { type: Date, default: Date.now() },
   updatedAt: Date,
-  tombstoned: Boolean,
-  tombstonedAt: Date.now(),
+  tombstoned: { type: Boolean, default: false },
+  tombstonedAt: { type: Date, default: null },
 });
 
-const user = model("User", userSchema);
-export default user;
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = bcrypt.genSalt(10);
+  this.password = bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.method.comparePassword = (userPassword) => {
+  return bcrypt.compare(userPassword, this.password);
+};
+
+const User = model("User", userSchema);
+export default User;
